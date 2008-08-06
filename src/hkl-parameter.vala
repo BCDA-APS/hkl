5bd2043fea9b6d0f505a945b74cfc8da3889a6c0
Frédéric-Emmanuel PICCA <picca@synchrotron-soleil.fr>
2008-08-06 15:49:30 +0200

    * now parameter

---
+++ src/hkl-parameter.vala
@@ -0,0 +1,22 @@
+public struct Hkl.Parameter {
+	public weak string name;
+	public Interval range;
+	public double value;
+	public bool to_fit;
+
+	/* becarefull only static name */
+	public Parameter(string name, double min, double value, double max, bool to_fit)
+	{
+		this.name = name;
+		this.range.min = min;
+		this.range.max = max;
+		this.value = value;
+		this.to_fit = to_fit;
+	}
+
+	public void randomize()
+	{
+		if (this.to_fit)
+			this.value = Random.double_range(this.range.min, this.range.max);
+	}
+}
--- test/hkl-test-parameter.c
+++ test/hkl-test-parameter.c
@@ -1,6 +1,7 @@
 #include <math.h>
 
 #include <hkl-parameter.h>
+#include <hkl-constants.h>
 
 #include "hkl-test.h"
 
@@ -9,62 +10,38 @@
 #endif
 #define HKL_TEST_SUITE_NAME parameter
 
-HKL_TEST_SUITE_FUNC(new)
-{
-	HklParameter *p;
-
-	HKL_ASSERT_POINTER_EQUAL(NULL, hkl_parameter_new("", 2, 1, 3, HKL_TRUE));
-	HKL_ASSERT_POINTER_EQUAL(NULL, hkl_parameter_new("", 2, 1, 3, HKL_TRUE));
-	HKL_ASSERT_POINTER_EQUAL(NULL, hkl_parameter_new("", 2, 1, 3, HKL_TRUE));
-	HKL_ASSERT_POINTER_EQUAL(NULL, hkl_parameter_new("toto", 2, 1, 3, HKL_TRUE));
-
-	p = hkl_parameter_new("toto", 1, 2, 3, HKL_TRUE);
-	HKL_ASSERT_EQUAL(0, !p);
-	HKL_ASSERT_DOUBLES_EQUAL(1., p->range.min, HKL_EPSILON);
-	HKL_ASSERT_DOUBLES_EQUAL(2., p->value, HKL_EPSILON);
-	HKL_ASSERT_DOUBLES_EQUAL(3., p->range.max, HKL_EPSILON);
-	HKL_ASSERT_EQUAL(HKL_TRUE, p->not_to_fit);
-
-	hkl_parameter_free(p);
-
-	return HKL_TEST_PASS;
-}
-
-HKL_TEST_SUITE_FUNC(new_copy)
-{
-	HklParameter *copy, p;
-
-	hkl_parameter_set(&p, "toto", 1, 2, 3, HKL_TRUE);
-	copy = hkl_parameter_new_copy(&p);
-
-	HKL_ASSERT_POINTER_EQUAL(copy->name, p.name);
-	HKL_ASSERT_DOUBLES_EQUAL(copy->range.min, p.range.min, HKL_EPSILON);
-	HKL_ASSERT_DOUBLES_EQUAL(copy->value, p.value, HKL_EPSILON);
-	HKL_ASSERT_DOUBLES_EQUAL(copy->range.max, p.range.max, HKL_EPSILON);
-	HKL_ASSERT_EQUAL(copy->not_to_fit, p.not_to_fit);
-
-	hkl_parameter_free(copy);
-
-	return HKL_TEST_PASS;
-}
-
-HKL_TEST_SUITE_FUNC(set)
+HKL_TEST_SUITE_FUNC(init)
 {
 	HklParameter p;
 
-	HKL_ASSERT_EQUAL(HKL_FAIL, hkl_parameter_set(&p, "", 2, 1, 3, HKL_TRUE));
-	HKL_ASSERT_EQUAL(HKL_FAIL, hkl_parameter_set(&p, "", 2, 1, 3, HKL_TRUE));
-	HKL_ASSERT_EQUAL(HKL_FAIL, hkl_parameter_set(&p, "", 2, 1, 3, HKL_TRUE));
-	HKL_ASSERT_EQUAL(HKL_FAIL, hkl_parameter_set(&p, "toto", 2, 1, 3, HKL_TRUE));
-	HKL_ASSERT_EQUAL(HKL_SUCCESS, hkl_parameter_set(&p, "toto", 1, 2, 3, HKL_TRUE));
+	hkl_parameter_init(&p, "toto", 1., 2., 3., TRUE);
+	HKL_ASSERT_DOUBLES_EQUAL(1., p.range.min, hkl_EPSILON);
+	HKL_ASSERT_DOUBLES_EQUAL(2., p.value, hkl_EPSILON);
+	HKL_ASSERT_DOUBLES_EQUAL(3., p.range.max, hkl_EPSILON);
+	HKL_ASSERT_EQUAL(TRUE, p.to_fit);
+
+	return HKL_TEST_PASS;
+}
+
+HKL_TEST_SUITE_FUNC(copy)
+{
+	HklParameter copy, p;
+
+	hkl_parameter_init(&p, "toto", 1, 2, 3, TRUE);
+	copy = p;
+
+	HKL_ASSERT_POINTER_EQUAL(copy.name, p.name);
+	HKL_ASSERT_DOUBLES_EQUAL(copy.range.min, p.range.min, hkl_EPSILON);
+	HKL_ASSERT_DOUBLES_EQUAL(copy.value, p.value, hkl_EPSILON);
+	HKL_ASSERT_DOUBLES_EQUAL(copy.range.max, p.range.max, hkl_EPSILON);
+	HKL_ASSERT_EQUAL(copy.to_fit, p.to_fit);
 
 	return HKL_TEST_PASS;
 }
 
 HKL_TEST_SUITE_BEGIN
 
-HKL_TEST( new );
-HKL_TEST( new_copy );
-HKL_TEST( set );
+HKL_TEST( init );
+HKL_TEST( copy );
 
 HKL_TEST_SUITE_END
